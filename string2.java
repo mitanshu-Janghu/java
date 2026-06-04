8b5234760586ff6b5510dbcff8c32650012da372
madara uchiha <notwhiten444@gmail.com>
2026-06-04 14:00:35 +0530

    -

---
+++ string2.java
@@ -0,0 +1,8 @@
+public class string2 {
+    public static void main(String[] args) {
+        String a ="mitanshu";
+        String b ="mitanshu";
+        System.out.println(a.equals(b));
+//String.equals();
+    }
+}
