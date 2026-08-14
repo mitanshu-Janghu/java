4b4527ff5a38866a977eed9ecc88ee04ecdeac39
madara uchiha <notwhiten444@gmail.com>
2026-08-14 23:39:01 +0530

    -

---
+++ reverseofll.java
@@ -0,0 +1,18 @@
+public class reverseofll {
+//      class Solution {
+//     public ListNode reverseList(ListNode head) {
+//         if(head == null || head.next==null) return head;
+//       ListNode pre = null;
+//       ListNode cur = head;
+//       ListNode next= cur.next;
+//       while(cur!=null){
+//         cur.next = pre;
+//         pre = cur;
+//         cur = next;
+//         if(next!=null)
+//         next= next.next;
+//       }
+//       head =pre;
+//    return head; }
+//  }
+}
