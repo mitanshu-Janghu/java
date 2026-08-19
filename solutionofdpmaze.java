ad86750d8b28827b19643edd113184aa32ed8b12
madara uchiha <notwhiten444@gmail.com>
2026-08-19 22:45:05 +0530

    -

---
+++ solutionofdpmaze.java
@@ -0,0 +1,22 @@
+public class solutionofdpmaze {
+   public int numberOfPaths(int m, int n) {
+         int[][] dp = new int[m+1][n+1];
+       for(int i= 0 ; i<=m ;i++)
+       for(int j = 0 ; j<=n;j++)
+       dp[i][j]=-1;
+       
+       return dpnumberOfPaths(m,n,dp);
+        
+    }
+    public int dpnumberOfPaths(int m, int n,int[][] dp) {
+        if(m==1 && n==1) return 1;
+        if(m==0 || n==0) return 0;
+        if(dp[m][n]!=-1) return dp[m][n];
+        int up = dpnumberOfPaths( m-1,  n,dp);
+        int down = dpnumberOfPaths( m,  n-1,dp);
+        return dp[m][n]=up+down;
+    }
+    public static void main(String[] args) {
+        
+    } 
+}
